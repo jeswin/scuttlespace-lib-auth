8d278d8de90d17f2c5e4155550848e01d4a5e258
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-09 13:00:04 +0530

    Adjust col size

--- src/setup.ts
+++ src/setup.ts
@@ -1,11 +1,11 @@
 export default async function setup() {
   const account = [
     `CREATE TABLE account (
-      id BIGSERIAL PRIMARY KEY,
+      rowid BIGSERIAL PRIMARY KEY,
       username VARCHAR(64) UNIQUE NOT NULL,
       external_id VARCHAR(64) UNIQUE NOT NULL,
       enabled BOOLEAN NOT NULL,
-      domain VARCHAR(64),
+      domain VARCHAR(128),
       about TEXT
     )`,
     `CREATE INDEX account_username_index ON account(username)`,
@@ -15,10 +15,10 @@ export default async function setup() {
 
   const accountPermissions = [
     `CREATE TABLE account_permissions ( 
-      id BIGSERIAL PRIMARY KEY,
+      rowid BIGSERIAL PRIMARY KEY,
       assignee_external_id VARCHAR(64) NOT NULL REFERENCES account(external_id),
       external_id VARCHAR(64) NOT NULL REFERENCES account(external_id),
-      permissions VARCHAR(512) NOT NULL
+      permissions TEXT NOT NULL
     )`
   ];
 
