306981f96aae479cec8be29632f4430c48161139
Jeswin Kumar <jeswinpk@agilehead.com>
2018-08-16 10:56:49 +0530

    Update npmlink

--- npmlink.sh
+++ npmlink.sh
@@ -1,5 +1,13 @@
-rm -rf ../scuttlespace-service-common/node_modules
-npm link ../scuttlespace-service-common
+set -e 
+BASHO_VERSION=$(basho -v | basho 'x.split(".")[2]' -j 'parseInt(x) >= 43 && "OK"')
 
-rm -rf ../scuttlespace-service-user-graphql-schema/node_modules
-npm link ../scuttlespace-service-user-graphql-schema
+if [[ $BASHO_VERSION == "OK" ]]
+then
+  (
+    echo scuttlespace-service-common &&
+    echo scuttlespace-service-user-graphql-schema
+  ) | basho -n proj -e 'cd ../${x} && npm link && [ -f npmlink.sh ] && ./npmlink.sh || echo skipped' \
+        -s proj -j '`update ${x}`'
+else
+  echo "Install basho version 0.0.43 or higher."
+fi
