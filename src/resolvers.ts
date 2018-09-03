6cc536e285faa40ddd2229268950be89b5731916
Jeswin Kumar <jeswinpk@agilehead.com>
2018-09-03 13:26:55 +0530

    WIP

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,5 @@
 import { parseServiceResult } from "scuttlespace-service-common";
-import { types } from "scuttlespace-service-user-graphql-schema";
+import * as  types from "scuttlespace-service-user-graphql-schema";
 import { createOrRenameUser, enableUser, findUser } from "./user";
 
 export default {
