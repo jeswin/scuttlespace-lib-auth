c7eafdc6b2f78a5e138e8ec71668ad0ab11d9109
Jeswin Kumar <jeswinpk@agilehead.com>
2018-09-03 20:34:04 +0530

    WIP

--- tools/codegen/generateResolvers.ts
+++
@@ -1,4 +0,0 @@
-import * as graphqlToTS from "graphql-to-ts";
-import * as gqlSchema from "scuttlespace-service-user-graphql-schema";
-
-console.log(graphqlToTS.getTypes(gqlSchema.schema));
---
+++ tools/generateResolvers.ts
@@ -0,0 +1,42 @@
+import * as graphqlToTS from "graphql-to-ts";
+import gqlSchema from "scuttlespace-service-user-graphql-schema";
+import { inspect } from "util";
+
+const resolvers = `
+  import { parseServiceResult } from "scuttlespace-service-common";
+  import * as types from "scuttlespace-service-user-graphql-schema";
+  import { createOrRenameUser, enableUser, findUser } from "./user";
+
+  export default {
+    Mutation: {
+      async createOrRenameUser(
+        root: any,
+        args: { input: types.ICreateOrRenameUserArgs },
+        context: any
+      ): Promise<string | undefined> {
+        const result = await createOrRenameUser(args.input, context);
+        return await parseServiceResult(result);
+      },
+      async enableUser(
+        root: any,
+        args: { input: types.IChangeUserStatusArgs },
+        context: any
+      ): Promise<types.IChangeUserStatusResult | undefined> {
+        const result = await enableUser(args.input.externalId, context);
+        return await parseServiceResult(result);
+      }
+    },
+    Query: {
+      async user(
+        root: any,
+        args: { domain: string; rowid: string },
+        context: any
+      ): Promise<types.IScuttlespaceUserDTO | undefined> {
+        const result = await findUser(args, context);
+        return await parseServiceResult(result);
+      }
+    }
+  };
+`;
+
+console.log(inspect(graphqlToTS.getTypes(gqlSchema), undefined, 8));
